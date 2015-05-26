27d79df4484891ec10da1b9189a5e14b22a15327
karatekid <mikechri@umich.edu>
2015-05-26 19:56:09 +0900

    added functionality

--- include/board.h
+++ include/board.h
@@ -2,6 +2,7 @@
 #define __BOARD__H__
 
 #include<string>
+#include<cstring>
 #include<vector>
 
 class Board {
@@ -9,16 +10,18 @@ class Board {
 		Board();
 		Board(char * initString);
 		Board(const Board &b);
-		Board & operator =(const Board b);
+		Board & operator =(const Board &b);
 
-		bool operator ==(Board b);
+		bool operator ==(const Board &b);
 
 		std::string serialize();
+		std::string print();
+
 		enum Move { U = 0, R = 1, D = 2, L = 3};
 		static const char * const MoveNames[]; 
 
-		class InvalidMoveError;
-		class InvalidBoardError;
+		class InvalidMoveError{};
+		class InvalidBoardError{};
 
 		std::vector<Move> getPossibleMoves();
 		void makeMove(Move m);
@@ -31,5 +34,7 @@ class Board {
 		int pos;
 		char *layout;
 		std::vector<Move> movesMade;
+
+		void posSwap(int newPos);
 };
 #endif
--- lib/board.cpp
+++ lib/board.cpp
@@ -16,6 +16,69 @@
 Board::Board()
 	:layout("01234567_"), pos(9){
 		 movesMade = std::vector<Move>();
+}
+
+Board::Board(char * initString){
+	Board();
+	layout = initString;
+	pos = strstr(layout, "_") - layout;
+	if(pos < 0) { 
+		throw InvalidBoardError();
 	}
+	//TODO: Verify good input
+}
+
+bool Board::operator ==(const Board &b) {
+	return strcmp(b.layout,layout) == 0;
+}
+
+std::string Board::serialize() {
+	return std::string(layout);
+}
+std::string Board::print() {
+	std::string s = "";
+	int x = 0, y = 0;
+	for(int i = 0; i < 9; ++i) {
+		if(i != 0 && i%3 == 0) {
+			s += '\n';
+		}
+		s += layout[i];
+	}
+	return s;
+}
+
+std::vector<Board::Move> Board::getPossibleMoves() {
+	std::vector<Move> moves = std::vector<Move>();
+	if(pos/3 > 0) {
+		moves.push_back(U);
+	} if(pos/3 < 2) {
+		moves.push_back(D);
+	} if(pos%3 > 0) {
+		moves.push_back(L);
+	} if(pos%3 < 2) {
+		moves.push_back(R);
+	}
+	return moves;
+}
+
+void Board::makeMove(Move m) {
+	//TODO: check for invalid moves
+	if(m == U) {
+		posSwap(pos-3);
+	} else if(m == D) {
+		posSwap(pos+3);
+	} else if(m == L) {
+		posSwap(pos-1);
+	} else if(m == R) {
+		posSwap(pos+1);
+	}
+}
+
+void Board::posSwap(int newPos) {
+	char temp = layout[pos];
+	layout[pos] = layout[newPos];
+	layout[newPos] = temp;
+	pos = newPos;
+}
 
 const char * const Board::MoveNames[] = {"U", "R", "D", "L"}; 
