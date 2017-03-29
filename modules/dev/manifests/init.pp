3f4655a7517ec26cb7ec386ed909818d2f05d4dc
karatekid <mchristen96@gmail.com>
2017-03-28 23:43:47 -0700

    Git configuration is failing on execution

--- modules/dev/manifests/init.pp
+++ modules/dev/manifests/init.pp
@@ -25,4 +25,83 @@ class dev(
         'kchmck/vim-coffee-script',
     ],
   }
+	include git
+  git::config { 'core.editor':
+	  value => 'vim',
+	}
+  # git::config { 'alias.st':
+	#   value => 'status --short --branch',
+  #   user => $user,
+	# }
+  # git::config { 'alias.ci':
+	#   value => 'commit',
+  #   user => $user,
+	# }
+  # git::config { 'alias.br':
+	#   value => 'branch',
+  #   user => $user,
+	# }
+  # git::config { 'alias.co':
+	#   value => 'checkout',
+  #   user => $user,
+	# }
+  # git::config { 'alias.lo':
+	#   value => 'log --oneline',
+  #   user => $user,
+	# }
+  # git::config { 'alias.log-detail':
+	#   value => 'log -p',
+  #   user => $user,
+	# }
+  # git::config { 'alias.graph':
+	#   value => 'log --graph --oneline --all',
+  #   user => $user,
+	# }
+  # # Force push, but stop from overwriting future state
+  # git::config { 'alias.please':
+	#   value => 'push --force-with-lease',
+  #   user => $user,
+	# }
+  # # Add file to previous commit
+  # git::config { 'alias.commend':
+	#   value => 'commit --amend --no-edit',
+  #   user => $user,
+	# }
+	# # Only unstaged changes
+  # git::config { 'stsh':
+	# 	value => 'stash --keep-index',
+  #   user => $user,
+  # }
+	# # untracked & tracked
+  # git::config { 'staash':
+	# 	value => 'stash --include-untracked',
+  #   user => $user,
+  # }
+	# # ignored, untracked, tracked
+  # git::config { 'staaash':
+	# 	value => 'stash --all',
+  #   user => $user,
+  # }
+  # # Sweet graph
+  # git::config { 'grog':
+	# 	value => 'log --graph --oneline --abbrev-commit --decorate --all',
+  #   user => $user,
+  # }
+  # # Clean merged branches from repo
+  # git::config { 'clean-remote':
+	# 	value => "!for b in $(git br -r --merged master); do echo $b | awk '{split($0,a,\"/\"); print a[2]}' | xargs git push origin --delete; done",
+  #   user => $user,
+  # }
+  # git::config { 'list-remote':
+	# 	value => "!for b in $(git br -r --merged master); do echo $b | awk '{split($0,a,\"/\"); print a[2]}'; done",
+  #   user => $user,
+  # }
+  # git::config { 'clean-local':
+	# 	value => "!git br --merged master | grep -v master | xargs git br -d",
+  #   user => $user,
+  # }
+  # git::config { 'list-local':
+	# 	value => "!git br --merged master | grep -v master",
+  #   user => $user,
+  # }
 }
